8dcee565f6b8161b5a25932179ffc2ee923965ff
Yojan Pardo <Yojan.Pardo@gmail.com>
2018-11-29 12:32:31 -0500

    agrego vista del login pero no hace nada

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('ordering/',local_views.ordering),
     path('access/<str:name>/<int:age>',local_views.access),
     path('posts/',posts_views.posts),
+    path('login/',users_views.login_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def login_view(request):
+    if request.method=='POST':
+        print('='*10)
+
+        print('='*10)
+    return render(request,'users/login.html')
