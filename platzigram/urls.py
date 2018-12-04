920c1178d181ca679646f757111f5f3f702509ba
Yojan Pardo <Yojan.Pardo@gmail.com>
2018-12-04 00:28:08 -0500

    agrego las validaciones del servidor a los formularios de actualización de usuarios

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -7,12 +7,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-	path('', local_views.home, name = 'home'),
     path('admin/', admin.site.urls, name = 'admin_site'),
     path('hello-world/',local_views.hello_world, name = 'hello_world'),
     path('ordering/',local_views.ordering, name='sort'),
     path('access/<str:name>/<int:age>',local_views.access, name = 'access'),
-    path('posts/',posts_views.posts, name = 'posts'),
+    path('',posts_views.posts, name = 'posts'),
     path('login/',users_views.login_view, name = 'login'),
     path('logout/',users_views.logout_view, name='logout'),
     path('sign_up/',users_views.sign_up_view, name='sign_up'),
